a2d4caa0b63577ed937e6abbfc8e4f6decad167c
parth99p <parthpatil4999@gmail.com>
2026-07-21 09:53:04 +0530

    Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,11 @@ import friedrice from "../assets/friedrice.jpg";
 import fries from "../assets/fries.jpg";
 import meduvada from "../assets/meduvada.jpg";
 import misalpav from "../assets/misalpav.jpg";
-import momos from "../assets/momos.jpg";
+import momo from "../assets/momo.jpg";
 import paneerchilly from "../assets/paneerchilly.jpg";
 import paneercrispy from "../assets/paneercrispy.jpg";
 import paneermasala from "../assets/paneermasala.jpg";
-import puribhaji from "../assets/puribhaaji.jpg";
+import puribhaji from "../assets/puribhaji.jpg";
 import samosa from "../assets/samosa.jpg";
 import sandwich from "../assets/sandwich.jpg";
 import sevbhaji from "../assets/sevbhaji.jpg";
@@ -21,11 +21,129 @@ import vegfriedrice from "../assets/vegfriedrice.jpg";
 import vegnoodles from "../assets/vegnoodles.jpg";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+
+let cardData = [
+  {
+    id: 1,
+    name : "Dalfry",
+    image : dalfry,
+    price : 80,
+  },
+  {
+    id: 2,
+    name : "Dalkhichdi",
+    image : dalkhichdi,
+    price : 120,
+  },
+  {
+    id: 3,
+    name : "Fried Rice",
+    image : friedrice,
+    price : 100,
+  },
+  {
+    id: 4,
+    name : "Fries",
+    image : fries,
+    price : 60,
+  },
+  {
+    id: 5,
+    name : "Meduvada",
+    image : meduvada,
+    price : 50,
+  },
+  {
+    id: 6,
+    name : "Misal Pav",
+    image : misalpav,
+    price : 150,
+  },
+  {
+    id: 7,
+    name : "Momo",
+    image : momo,
+    price : 120,
+  },
+  {
+    id: 8,
+    name : "Paneer Chilly",
+    image : paneerchilly,
+    price : 180,
+  },
+  {
+    id: 9,
+    name : "Paneer Crispy",
+    image : paneercrispy,
+    price : 200,
+  },
+  {
+    id: 10,
+    name : "Paneer Masala",
+    image : paneermasala,
+    price : 180,
+  },
+  {
+    id: 11,
+    name : "Puri Bhaji",
+    image : puribhaji,
+    price : 100,
+  },
+  {
+    id: 12,
+    name : "Samosa",
+    image : samosa,
+    price : 50,
+  },
+  {
+    id: 13,
+    name : "Sandwich",
+    image : sandwich,
+    price : 80,
+  },
+  {
+    id: 14,
+    name : "Sev Bhaji",
+    image : sevbhaji,
+    price : 120,
+  },
+  {
+    id: 15,
+    name : "Tea",
+    image : tea,
+    price : 20,
+  },
+  {
+    id: 16,
+    name : "Vada Saus",
+    image : vadausal,
+    price : 60,
+  },
+  {
+    id: 17,
+    name : "Veg Biryani",
+    image : vegbiryani,
+    price : 150,
+  },
+  {
+    id: 18,
+    name : "Veg Fried Rice",
+    image : vegfriedrice,
+    price : 120,
+  },
+  {
+    id: 19,
+    name : "Veg Noodles",
+    image : vegnoodles,
+    price : 100,
+  }
+];
+
+return (
+  <div>
+    <h1>Home</h1>
+  </div>
+);
 };
 
-export default Home;
+export default Home; 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "../components/Navbar.css";
+import { IoMdCart } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <div className='mainContainer'>
+      <nav>
+        <div className='logo'>
+          <div className='foodLogo'>🍗</div>
+          <div>Indian Masala Restaurant</div>
+      </div>
+    <ul className='listItems'>
+      <li> <Link to={'/'}>Home</Link> </li>
+      <li> <Link to={'/about'}>About</Link> </li>
+      <li> <Link to={'/contact'}>Contact</Link> </li>
+      <li>
+        <Link to ={''}> <IoMdCart /></Link>
+      
+      </li>
+    </ul>
+    </nav>
+</div>
+
+   
   );
 };
 
