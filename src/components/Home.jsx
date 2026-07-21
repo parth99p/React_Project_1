8ce919e2c8e4cebf8f893f1842917c99abedec02
parth99p <parthpatil4999@gmail.com>
2026-07-21 09:08:22 +0530

    jpg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 import "../components/Home.css";
+import dalfry from "../assets/dalfry.jpg";
+import dalkhichdi from "../assets/dalkhichdi.jpg";
+import friedrice from "../assets/friedrice.jpg";
+import fries from "../assets/fries.jpg";
+import meduvada from "../assets/meduvada.jpg";
+import misalpav from "../assets/misalpav.jpg";
+import momos from "../assets/momos.jpg";
+import paneerchilly from "../assets/paneerchilly.jpg";
+import paneercrispy from "../assets/paneercrispy.jpg";
+import paneermasala from "../assets/paneermasala.jpg";
+import puribhaji from "../assets/puribhaaji.jpg";
+import samosa from "../assets/samosa.jpg";
+import sandwich from "../assets/sandwich.jpg";
+import sevbhaji from "../assets/sevbhaji.jpg";
+import tea from "../assets/tea.jpg";
+import vadausal from "../assets/vadausal.jpg";
+import vegbiryani from "../assets/vegbiryani.jpg";
+import vegfriedrice from "../assets/vegfriedrice.jpg";
+import vegnoodles from "../assets/vegnoodles.jpg";
 
 const Home = () => {
   return (
