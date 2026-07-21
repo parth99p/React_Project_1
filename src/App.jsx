2b62125f6e91bb7ff49d8cfc188b60e1191918e9
parth99p <parthpatil4999@gmail.com>
2026-07-21 10:18:00 +0530

    new css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import "./index.css";
 
 const App = () => {
   return (
