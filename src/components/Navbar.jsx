aa78acef4e88b69aebd702e89d56a4d86700f834
parth99p <parthpatil4999@gmail.com>
2026-07-21 10:14:08 +0530

    css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       <li> <Link to={'/about'}>About</Link> </li>
       <li> <Link to={'/contact'}>Contact</Link> </li>
       <li>
-        <Link to ={''}> <IoMdCart /></Link>
+        <Link to ={'/cart'}> <IoMdCart /></Link>
       
       </li>
     </ul>
